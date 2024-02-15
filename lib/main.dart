2439c9fb1535cad1e5eaa8e6563a3aee2225d371
AdekunleAriyo <dekunlearry@gmail.com>
2024-02-15 20:39:15 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Animated Login',
+      title: 'Flutter Animated Login Page',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
